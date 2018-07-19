1dc7321eab8d3f6522032c720991c6a43b6f5274
Dan Sensecall <dan@sensecall.co.uk>
2018-07-19 09:13:05 +0100

    Content and layout updates

--- app/views/non-gds/version-1/routes.js
+++ app/views/non-gds/version-1/routes.js
@@ -2,8 +2,14 @@ const express = require('express')
 
 const router = new express.Router()
 
-// router.get('/', (req, res) => {
-//   res.redirect(`/${req.version}/`)
-// })
+router.get('/what-is-an-apprenticeship', (req, res) => {
+	var taskStep = '1'
+	res.render(`${req.feature}/${req.sprint}/what-is-an-apprenticeship`,{taskStep})
+})
+
+router.get('/apprenticeship-costs', (req, res) => {
+	var taskStep = '2'
+	res.render(`${req.feature}/${req.sprint}/apprenticeship-costs`,{taskStep})
+})
 
 module.exports = router
