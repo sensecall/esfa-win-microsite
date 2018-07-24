6d45225e4182629a0e19acf43fa8566eda72da2d
Dan Sensecall <dan@sensecall.co.uk>
2018-07-24 09:07:19 +0100

    Content

--- app/views/branching/version-1/routes.js
+++ app/views/branching/version-1/routes.js
@@ -2,14 +2,26 @@ const express = require('express')
 
 const router = new express.Router()
 
-router.get('/what-is-an-apprenticeship', (req, res) => {
-	var taskStep = '1'
-	res.render(`${req.feature}/${req.sprint}/what-is-an-apprenticeship`,{taskStep})
+router.get('/', (req, res) => {
+	res.render(`${req.feature}/${req.sprint}/list`)
 })
 
 router.get('/apprenticeship-costs', (req, res) => {
 	var taskStep = '2'
 	res.render(`${req.feature}/${req.sprint}/apprenticeship-costs`,{taskStep})
+})
+
+// Branching
+router.get('/costs-levy-check', function (req, res) {
+	var levy = req.query.levy
+
+	if (levy === 'false') {
+		res.redirect(`/${req.feature}/${req.sprint}/non-levy`)
+	} else if (levy === 'true') {
+		res.redirect(`/${req.feature}/${req.sprint}/levy-payer`)
+	} else {
+		res.render(`${req.feature}/${req.sprint}/costs-levy-check`)
+	}
 })
 
 module.exports = router
